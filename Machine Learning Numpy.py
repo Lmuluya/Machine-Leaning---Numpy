abe1e9f9ad4d3756d52615b6f60638a1b02fdd85
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-09-20 22:34:46 +0300

    Add files via upload

--- Machine Learning Numpy.py
+++ Machine Learning Numpy.py
@@ -1,14 +1,21 @@
 # Creating a numpy array
+# Using the ndaray objects
 import numpy as np
 a = np.arange(6).reshape(2,3)
 print(a)
 # The dimension of array a
+print("Dimension of the array"); print(a.ndim)
+print("Shape of the array"); print(a.shape)
+print("Size of the array");print(a.size)
+print("Type of the array"); print(a.dtype)
+print("Item size"); print(a.itemsize)
 
-print("Dimension of the array")
-print(a.ndim)
-
-print("Shape of the array")
-print(a.shape)
+## Attributes of an array
+c = np.array(([1,2,3]),dtype=complex)
+print(c)
+print("Item size"); print(c.itemsize)
+print(np.zeros((2,3)))
+print(np.zeros((3,3),dtype=np.int16))
 
 
 
